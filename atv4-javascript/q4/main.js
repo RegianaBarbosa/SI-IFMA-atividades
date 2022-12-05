57b35777925e36b5be97f5e83691d870b5b1ec6e
“RegianaBarbosa” <regiana.cruz@cad.ifma.edu.br>
2022-12-05 09:38:45 -0300

    getListNome commit

--- atv4-javascript/q4/main.js
+++ atv4-javascript/q4/main.js
@@ -50,34 +50,44 @@ calcButton.addEventListener('click', e => {
     let checkDocSalg = document.getElementsByName('doceSalg');
     console.log(checkDocSalg)
     for(let i=0; i<checkDocSalg.length;i++){
-        if (checkDocSalg[i].checked == true) {
-            switch (checkDocSalg[i].value) {
-                case 'bolo':
-                    pedido.add(bolo)
-                    break;
-                case 'pastel':
-                    pedido.add(pastel)
-                    break;
-                case 'torta':
-                    pedido.add(torta)
-                    break;
+        if (checkDocSalg != null) {
+            if (checkDocSalg[i].checked == true) {
+                switch (checkDocSalg[i].value) {
+                    case 'bolo':
+                        pedido.add(bolo)
+                        break;
+                    case 'pastel':
+                        pedido.add(pastel)
+                        break;
+                    case 'torta':
+                        pedido.add(torta)
+                        break;
+                }
+            } else{
+                console.error("Item "+ i + "não marcado");
             }
         } else {
             console.error("Item não encontrado");
         }
     }
+    let itens
+    itens = pedido.getItens().join(', ')
+    console.log(itens)
 
     //imprimir resultado
     window.alert('Nome do Cliente: ' + nome
+        + "\nItens Consumidos: " + pedido.getListNome() 
         + "\nTotal a pagar: R$ " + parseFloat(pedido.getTotal()).toFixed(2) + " reais")
 });
 
 function criarPedido() {
     let pedido = {
         itens: [],
+        nomeItens: [],
 
         add: function (item) {
             this.itens.push(item)
+            this.nomeItens.push(item.getNome())
         },
 
         getTotal: function () {
@@ -86,8 +96,13 @@ function criarPedido() {
                 total = total + item.getPreco()
             })
             return total
+        },
+
+        getListNome: function () {
+            const nomes = this.nomeItens.join(', ')            
+            return console.log(nomes)
         }
     }
-    return pedido
+    return pedido 
 }
 
