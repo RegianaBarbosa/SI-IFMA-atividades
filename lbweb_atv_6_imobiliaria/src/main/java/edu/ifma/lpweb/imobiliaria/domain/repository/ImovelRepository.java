34f855a6aaae4e27f3c34cfbd492b4e1e1f33788
“RegianaBarbosa” <regiana.cruz@cad.ifma.edu.br>
2022-12-24 18:13:43 -0300

    imports repository

--- lbweb_atv_6_imobiliaria/src/main/java/edu/ifma/lpweb/imobiliaria/domain/repository/ImovelRepository.java
+++ lbweb_atv_6_imobiliaria/src/main/java/edu/ifma/lpweb/imobiliaria/domain/repository/ImovelRepository.java
@@ -1,5 +1,10 @@
 package edu.ifma.lpweb.imobiliaria.domain.repository;
 
-public class ImovelRepository {
+import edu.ifma.lpweb.imobiliaria.domain.model.Imovel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImovelRepository extends JpaRepository<Imovel, Integer>{
     
 }
--- lbweb_atv_6_imobiliaria/src/main/java/edu/ifma/lpweb/imobiliaria/domain/repository/LocacaoRepository.java
+++ lbweb_atv_6_imobiliaria/src/main/java/edu/ifma/lpweb/imobiliaria/domain/repository/LocacaoRepository.java
@@ -1,5 +1,10 @@
 package edu.ifma.lpweb.imobiliaria.domain.repository;
 
-public class LocacaoRepository {
+import edu.ifma.lpweb.imobiliaria.domain.model.Locacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocacaoRepository extends JpaRepository<Locacao, Integer>{
     
 }
