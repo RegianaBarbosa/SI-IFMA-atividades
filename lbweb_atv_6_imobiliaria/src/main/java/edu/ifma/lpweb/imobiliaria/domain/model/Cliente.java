3d23490b291a1397c012d36d92ba1a0c8f4727ea
“RegianaBarbosa” <regiana.cruz@cad.ifma.edu.br>
2022-12-24 17:30:30 -0300

    model atributos e mapeamento

--- lbweb_atv_6_imobiliaria/src/main/java/edu/ifma/lpweb/imobiliaria/domain/model/Cliente.java
+++ lbweb_atv_6_imobiliaria/src/main/java/edu/ifma/lpweb/imobiliaria/domain/model/Cliente.java
@@ -1,5 +1,42 @@
 package edu.ifma.lpweb.imobiliaria.domain.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Getter @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Cliente {
-    
+    @EqualsAndHashCode.Include
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank @Size(max= 60)
+    private String nome;
+
+    @NotBlank
+    private String cpf;
+
+    @NotBlank @Size(min= 8, max= 20)
+    private String  telefone;
+
+    @NotBlank @Email @Size(max= 100)
+    private String email;
+
+    @NotBlank @DateTimeFormat
+    private Date dt_nasc;
+
 }
